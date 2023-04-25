f4c79b803bb197ee3c56e3207ed351aab5266ca7
Seidmani64 <A01028650@itesm.mx>
2023-04-25 15:25:59 -0600

    Added random encounters

--- Assets/Scripts/OverworldController.cs
+++ Assets/Scripts/OverworldController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OverworldController : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class OverworldController : MonoBehaviour
     private Vector3 move = Vector3.zero;
     private Vector3 goal = Vector3.zero;
     private bool moving = false;
+
+    private float randEncounter = 0f;
 
 
     void Start()
@@ -31,19 +34,29 @@ public class OverworldController : MonoBehaviour
     private void MoveTowardsGoal()
     {
         transform.position = Vector3.MoveTowards(transform.position, goal, speed * Time.deltaTime);
+        transform.LookAt(goal);
     }
 
     private void SetNewGoal()
     {
         hInput = Input.GetAxisRaw("Horizontal");
         vInput = Input.GetAxisRaw("Vertical");
-        
+
+        if(hInput != 0 || vInput != 0)
+            {
+                randEncounter = Random.Range(0f, 1f);
+                Debug.Log(randEncounter);
+                if(randEncounter < 0.03f) 
+                    SceneManager.LoadScene("Battle");
+            }
+
         Vector3 tempVect = Vector3.zero;
 
         if(Mathf.Abs(hInput) >= Mathf.Abs(vInput))
             tempVect = new Vector3(hInput, 0, 0);
         else
             tempVect  = new Vector3(0,0,vInput);
+
         
         goal = transform.position + tempVect.normalized;
     }
